e008100171a782408c4e92cb83913ee008285b4e
JoaqAmado <wacky.macoolit@gmail.com>
2026-01-13 15:39:31 -0500

    Completed grid creation and deletion

--- sketch.js
+++ sketch.js
@@ -1,18 +1,36 @@
 function createGrid(size){
-    let div;
+    let row;
     let square;
+    let container = document.createElement("div");
+    let body = document.querySelector("body");
+
+    container.classList.add("container");
     for (let i = 0; i<size; i++){
-        div = document.createElement("div");
-        div.style.display = "flex";
+        row = document.createElement("div");
+        row.classList.add("row");
         for (let j = 0; j<size; j++){
             square = document.createElement("div");
             square.classList.add("square");
-            div.appendChild(square);
+            row.appendChild(square);
         }
+        container.appendChild(row);
     }
+    body.appendChild(container);
 }
 
-let size = document.querySelector("#size");
-if (!Number.isInteger(Number(size.value))){
-    alert("Invalid Input!");
+function deleteGrid(){
+    let container = document.querySelectorAll("div");
+    container.forEach((div) => {
+        div.remove();
+    });
 }
+
+
+let enterButton = document.querySelector("button");
+let size = 16;
+createGrid(size);
+enterButton.addEventListener("click", () => {
+    size = Number(prompt("Enter new size!"));
+    deleteGrid();
+    createGrid(size);
+});
