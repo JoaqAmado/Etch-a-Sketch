4eb1252c4d723b629b7aa98e82d1304578a7a8a3
JoaqAmado <wacky.macoolit@gmail.com>
2026-01-13 13:57:27 -0500

    Completed input validation

--- sketch.js
+++ sketch.js
@@ -3,10 +3,16 @@ function createGrid(size){
     let square;
     for (let i = 0; i<size; i++){
         div = document.createElement("div");
+        div.style.display = "flex";
         for (let j = 0; j<size; j++){
             square = document.createElement("div");
             square.classList.add("square");
             div.appendChild(square);
         }
     }
+}
+
+let size = document.querySelector("#size");
+if (!Number.isInteger(Number(size.value))){
+    alert("Invalid Input!");
 }
